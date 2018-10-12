eceb65d9159f9a80aeca39b12426a8356f205e14
Владислав Павлов <vlpavlov58@gmail.com>
2018-10-12 14:30:09 +0300

    Homework 1 / Sorting

--- HW/HW/Program.cs
+++ HW/HW/Program.cs
@@ -10,6 +10,34 @@ namespace HW
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int[] array = new int[10];
+            int temp;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-100, 100);
+            }
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = array[i];
+                    }
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.ReadLine();
         }
     }
 }
