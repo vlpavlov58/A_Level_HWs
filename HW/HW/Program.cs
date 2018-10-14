ecc22cce2ea97dc0caa4ac5c2b7eec524e3e039a
Владислав Павлов <vlpavlov58@gmail.com>
2018-10-14 15:11:39 +0300

    Max sum of coloumn

--- HW/HW/Program.cs
+++ HW/HW/Program.cs
@@ -10,6 +10,59 @@ namespace HW
     {
         static void Main(string[] args)
         {
+            var matrix = GenerateMatrix(5, 5, 20);
+
+            DrawMatrix(matrix);
+
+            Sum(matrix);
+
+            
+
+            Console.ReadLine();
+        }
+
+        public static int[,] GenerateMatrix(int x, int y, int maxRandom = 10)
+        {
+            int[,] matrix = new int[x, y];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    matrix[i, j] = rnd.Next(0, maxRandom);
+                }
+            }
+
+            return matrix;
+        }
+
+        public static void Sum(int[,] matrix)
+        {
+            int[] sum = {0};
+
+            for (int j = 0; j < matrix.GetLength(0); j++)
+            {
+                for (int i = 0; i < matrix.GetLength(1); i++)
+                {
+                    
+                        sum[j] += matrix[i, j];
+                    Console.WriteLine(sum[j]);
+                }
+            }
+        }
+
+        public static void DrawMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{(matrix[i, j].ToString().Length < 2 ? matrix[i, j].ToString() + "  |" : matrix[i, j].ToString() + " |")}");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
